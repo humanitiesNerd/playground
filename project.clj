069c6a33eada576898b9fbf60b4cb0ba77357db7
humanitiesNerd <catonano@gmail.com>
2012-11-10 11:32:24 +0100

    fixed some others warning by lein-pedantic

--- project.clj
+++ project.clj
@@ -3,6 +3,25 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"] [cascalog "1.10.0"][incanter/incanter-core "1.3.0"]]
-  :profiles { :dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]}
+  :dependencies [
+                [org.clojure/clojure "1.4.0"] 
+                [cascalog "1.10.0"]
+                [incanter/incanter-core "1.3.0"]
+                ]
+  :profiles { :dev 
+                  {:dependencies [
+                                 [org.apache.hadoop/hadoop-core "0.20.2-dev" 
+                                 :exclusions [
+                                             [org.slf4j/slf4j-api] 
+                                             [org.slf4j/slf4j-log4j12] 
+                                             [log4j]
+                                             [commons-logging]
+                                             commons-codec
+                                             ]
+                                 ]            
+                                 ]
+                  }
+
   })
+
+
