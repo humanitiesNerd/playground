16f0018c6b4ec91a8895903f6ae8357e67aa8039
humanitiesNerd <catonano@gmail.com>
2012-11-21 18:35:17 +0100

    notes on where I am

--- src/playground/operations.clj
+++ src/playground/operations.clj
@@ -22,3 +22,9 @@
 ;;   (?<- (stdout) [?person] (person ?person)) 
 ;;   (?<- (stdout) [?persona] (person ?persona)) 
 ;;   (?<- (stdout) [?col1 ?col2 ?col3] (mymatrix ?col1 ?col2 ?col3)) 
+;;   (?<- (stdout) [?col1 ?col2 ?col3 ?output] (mymatrix ?col1 ?col2 ?col3) (dosum ?col2 :> ?output)) 
+
+;; my ruples are like [1 0 0] si identity receives 2 arguments but it expects one.
+This is because the parallelagg doesn´t deal with tuples, it passes single values around (to init-var and to combine-var)
+Here https://github.com/nathanmarz/cascalog/wiki/Guide-to-custom-operations 
+defmultibufferop and defparallelbuf are quoted and I hope those could be useful to me in this case
