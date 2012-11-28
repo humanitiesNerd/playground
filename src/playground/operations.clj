374303c4839f91e2524eebf98022b20bf6c78f9a
humanitiesNerd <catonano@gmail.com>
2012-11-28 12:53:35 +0100

    boh

--- src/playground/operations.clj
+++ src/playground/operations.clj
@@ -15,12 +15,8 @@
    transposed)
 ) 
 
-(def my-identity [first-argument second-argument]
+(defn my-identity [first-argument second-argument]
   [first-argument second-argument]
-)
-
-(def my-identity2 [first-argument second-argument]
-  [[first-argument second-argument][first-argument second-argument]]
 )
 
 (defparallelagg dosum :init-var #'my-identity :combine-var #'identity)
@@ -30,9 +26,10 @@
 ;;   (?<- (stdout) [?person] (person ?person)) 
 ;;   (?<- (stdout) [?persona] (person ?persona)) 
 ;;   (?<- (stdout) [?col1 ?col2 ?col3] (mymatrix ?col1 ?col2 ?col3)) 
-;;   (?<- (stdout) [?col1 ?col2 ?col3 ?output] (mymatrix ?col1 ?col2 ?col3) (dosum ?col2 :> ?output)) 
+;;   (?<- (stdout) [?col1 ?col2 ?col3 ?output] (mymatrix ?col1 ?col2 ?col3) (dosum ?col1 :> ?output)) 
+;;   (macroexpand-all '(?<- (stdout) [?col1 ?col2 ?col3 ?output] (mymatrix ?col1 ?col2 ?col3) (dosum ?col1 :> ?output)) )
 
-;; my ruples are like [1 0 0] si identity receives 2 arguments but it expects one.
-This is because the parallelagg doesn´t deal with tuples, it passes single values around (to init-var and to combine-var)
-Here https://github.com/nathanmarz/cascalog/wiki/Guide-to-custom-operations 
-defmultibufferop and defparallelbuf are quoted and I hope those could be useful to me in this case
+;; my tuples are like [1 0 0] so identity receives 2 arguments but it expects one.
+;;This is because the parallelagg doesn´t deal with tuples, it passes single values around (to init-var and to combine-var)
+;;Here https://github.com/nathanmarz/cascalog/wiki/Guide-to-custom-operations 
+;;defmultibufferop and defparallelbuf are quoted and I hope those could be useful to me in this case
