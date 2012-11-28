73eb421e9687dd49fe51e97139fff9d35c09be92
humanitiesNerd <catonano@gmail.com>
2012-11-28 13:12:36 +0100

    definiamo la query senza eseguirla, primo passaggio verso il reverse engineering

--- src/playground/operations.clj
+++ src/playground/operations.clj
@@ -1,6 +1,6 @@
 (ns playground.operations
   (:use [playground.mockdata])
-  (:require 
+  (:require
             [incanter.core :as i]
             ))
 
@@ -13,7 +13,7 @@
   []
   (let [transposed (i/trans mymatrix)]
    transposed)
-) 
+)
 
 (defn my-identity [first-argument second-argument]
   [first-argument second-argument]
@@ -22,14 +22,16 @@
 (defparallelagg dosum :init-var #'my-identity :combine-var #'identity)
 
 
-;;   (?<- (stdout) [?person] (age ?person 25)) 
-;;   (?<- (stdout) [?person] (person ?person)) 
-;;   (?<- (stdout) [?persona] (person ?persona)) 
-;;   (?<- (stdout) [?col1 ?col2 ?col3] (mymatrix ?col1 ?col2 ?col3)) 
-;;   (?<- (stdout) [?col1 ?col2 ?col3 ?output] (mymatrix ?col1 ?col2 ?col3) (dosum ?col1 :> ?output)) 
+;;   (?<- (stdout) [?person] (age ?person 25))
+;;   (?<- (stdout) [?person] (person ?person))
+;;   (?<- (stdout) [?persona] (person ?persona))
+;;   (?<- (stdout) [?col1 ?col2 ?col3] (mymatrix ?col1 ?col2 ?col3))
+;;   (?<- (stdout) [?col1 ?col2 ?col3 ?output] (mymatrix ?col1 ?col2 ?col3) (dosum ?col1 :> ?output))
 ;;   (macroexpand-all '(?<- (stdout) [?col1 ?col2 ?col3 ?output] (mymatrix ?col1 ?col2 ?col3) (dosum ?col1 :> ?output)) )
+(def query  (<- [?col1 ?col2 ?col3 ?output] (mymatrix ?col1 ?col2 ?col3) (dosum ?col1 :> ?output)))
+
 
 ;; my tuples are like [1 0 0] so identity receives 2 arguments but it expects one.
 ;;This is because the parallelagg doesn´t deal with tuples, it passes single values around (to init-var and to combine-var)
-;;Here https://github.com/nathanmarz/cascalog/wiki/Guide-to-custom-operations 
+;;Here https://github.com/nathanmarz/cascalog/wiki/Guide-to-custom-operations
 ;;defmultibufferop and defparallelbuf are quoted and I hope those could be useful to me in this case
