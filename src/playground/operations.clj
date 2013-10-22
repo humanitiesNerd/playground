af85e56017e5e6a8a3067113600974ecd462a508
Adriano <catonano@potiomkin.(none)>
2013-10-23 01:37:48 +0200

    a bit of pretty formatting

--- src/playground/operations.clj
+++ src/playground/operations.clj
@@ -119,12 +119,23 @@
 
 
 (defn convert-to-numbers [data-source-tap]
-  (<- [?age ?workclass ?fnlwgt ?education ?education-num ?marital-status ?occupation ?relationship
-                   ?race ?sex ?capital-gain ?capital-loss
-                   ?hours-per-week ?native-country ?income-treshold ] (data-source-tap :> ?age ?workclass ?fnlwgt ?education ?education-num
-                                                                                          ?marital-status ?occupation ?relationship ?race
-                                                                                          ?sex ?capital-gain ?capital-loss
-                                                                                          ?hours-per-week ?native-country ?income-treshold))
+  (<- [?age
+       ?workclass
+       ?fnlwgt
+       ?education
+       ?education-num
+       ?marital-status
+       ?occupation ?relationship
+       ?race ?sex
+       ?capital-gain
+       ?capital-loss
+       ?hours-per-week
+       ?native-country
+       ?income-treshold ]
+      (data-source-tap :> ?age ?workclass ?fnlwgt ?education ?education-num
+                       ?marital-status ?occupation ?relationship ?race
+                       ?sex ?capital-gain ?capital-loss
+                       ?hours-per-week ?native-country ?income-treshold))
 )
 
 (defn my-workflow []
