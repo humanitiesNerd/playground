82718e154650ac6ade2d38ce00ada73614d8d6c9
Adriano <catonano@potiomkin.(none)>
2013-10-23 01:58:57 +0200

    how the hell do I use the cascalog syntax ?

--- src/playground/operations.clj
+++ src/playground/operations.clj
@@ -132,7 +132,7 @@
        ?hours-per-week
        ?native-country
        ?income-treshold ]
-      (data-source-tap :> ?age ?workclass ?fnlwgt ?education ?education-num
+      (data-source-tap  ?age ?workclass ?fnlwgt ?education ?education-num
                        ?marital-status ?occupation ?relationship ?race
                        ?sex ?capital-gain ?capital-loss
                        ?hours-per-week ?native-country ?income-treshold))
